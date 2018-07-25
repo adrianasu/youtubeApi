3eca383cfab634e5018b507a6f7308ae5601f498
Adriana Suarez <agsmq@yahoo.com>
2018-07-25 11:18:03 -0500

    fixed img width. now max results is 6.

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 const youtube_search_url = "https://www.googleapis.com/youtube/v3/search";
 
 function getDataFromApi(searchTerm, callback) {
-		const settings = {
-			url: youtube_search_url,
-			data: {q: searchTerm,
-						key: "AIzaSyCeqMDyG-nlak99Pbi88_TATn2xc5WQZEE",
-						part: 'snippet',
-						type: 'video',
-						},
-			success: callback
-		};
-		$.ajax(settings);
+	const settings = {
+		url: youtube_search_url,
+		data: {
+			q: searchTerm,
+			key: "AIzaSyCeqMDyG-nlak99Pbi88_TATn2xc5WQZEE",
+			part: 'snippet',
+			type: 'video',
+			maxResults: 6,
+		},
+		success: callback
+	};
+	$.ajax(settings);
 }
 
 function displayYoutubeResults(data) {
 	const results = data.items.map(item =>
-			generateStringsWithResults(item));
+		generateStringsWithResults(item));
 	$('.js-search-results').html(results).removeClass("hide-it");
 }
 
@@ -27,17 +29,17 @@ function generateStringsWithResults(item) {
 }
 
 function watchSubmitButton() {
-//  watch for when the form is submitted
-		$('.js-form').on('submit', event => {
-			event.preventDefault();
+	//  watch for when the form is submitted
+	$('.js-form').on('submit', event => {
+		event.preventDefault();
 		// get the input text value submitted
-			let query = $(this).find('.js-query');
-			let querySearch = query.val();
-			query.val("");
+		let query = $(this).find('.js-query');
+		let querySearch = query.val();
+		query.val("");
 
 		// callback function to display results from youtube api 
-			getDataFromApi(querySearch, displayYoutubeResults);
-		});
+		getDataFromApi(querySearch, displayYoutubeResults);
+	});
 }
 
 $(watchSubmitButton);
